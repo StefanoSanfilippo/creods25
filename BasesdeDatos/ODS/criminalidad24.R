5d9d1d3e7686f4045fd096ec12f8db70fa871e58
StefanoSanfilippo <64667244+StefanoSanfilippo@users.noreply.github.com>
2025-07-22 12:06:59 +0200

    Add files via upload

--- BasesdeDatos/ODS/criminalidad24.R
+++ BasesdeDatos/ODS/criminalidad24.R
@@ -1,24 +1,23 @@
 library(dplyr)
 library(stringr)
-library(tidyr)
+#library(tidyr)
 library(readxl)
 
 # https://estadisticasdecriminalidad.ses.mir.es/publico/portalestadistico/datos.html?type=pcaxis&path=/DatosBalanceAnt/20244/&file=pcaxis
 
 # Portal criminalidad: https://estadisticasdecriminalidad.ses.mir.es/publico/portalestadistico/balances.html
 
-homicidis <- read_xlsx("BasesdeDatos/ODS16/09012.xlsx",
-                          skip = 6) 
-names(homicidis) <- c("Municipio", "Homicidios", "Intentos")   
+criminalidad24 <- read_xlsx("BasesdeDatos/ODS16/1409012.xlsx",
+                          skip = 6)
 
-
-homicidis <- homicidis %>% 
+criminalidad <- criminalidad24 |> 
+    mutate(Municipio= ...1) %>% 
     filter(grepl("^03|^12|^46", Municipio)) %>% 
     mutate(ine= str_sub(Municipio,1,5),
            Municipio= str_sub(Municipio,7,100)) %>% 
-    select(4,1:3)
+    select(4,3,2) %>% 
+    rename(Crim.Convencional.2024= `I. CRIMINALIDAD CONVENCIONAL`)
     
     
-
-
-writexl::write_xlsx(homicidis, "BasesdeDatos/ODS16/homicidis.xlsx")
+    
+ writexl::write_xlsx(criminalidad, "BasesdeDatos/ODS16/criminalidad.xlsx")
